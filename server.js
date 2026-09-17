f3134b80935069b9a6d465e1ecde5f8c84601454
Drill-X <20508402+Drill-X@users.noreply.github.com>
2026-09-16 23:46:24 -0400

    updated oauth callback url

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ passport.use(new LocalStrategy(async function verify(username, password, cb) {
 passport.use(new GitHubStrategy({
     clientID: `${process.env.GITHUB_CLIENT_ID}`,
     clientSecret: `${process.env.GITHUB_CLIENT_SECRET}`,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    callbackURL: "https://a3-dexter-haehnichen.onrender.com/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
